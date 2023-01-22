3ea8d84d30dc9801f722c89cbfa7d3d0a576118b
Mateusz Chwast <chwastmat@gmail.com>
2023-01-22 21:32:55 +0100

    implement encoder direction and steps

--- Src/encoder.c
+++ Src/encoder.c
@@ -2,14 +2,24 @@
 #include "logging.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "main.h"
+#include "tim.h"
 
 
 #define TASK_STACK_SIZE 256
 #define TASK_NAME "Encoder Task"
 #define TASK_PRIORITY 1
+#define TASK_DELAY_TIME pdMS_TO_TICKS(150)
+
+#define ENCODER_TIMER htim3	//should be 16-bit timer
+#define ENCODER_TIMER_HALF_RANGE (0xFFFF / 2)
+
+static Encoder_Movement encoderMovement;
 
 
 static void EncoderTask();
+static void UpdateEncoderState();
+static void LogEncoderParams();
 
 
 bool Setup_Encoder_CreateTask()
@@ -27,20 +37,98 @@ bool Setup_Encoder_CreateTask()
 
 Encoder_Movement Encoder_GetMovement()
 {
-
+	return encoderMovement;
 }
 
 bool Encoder_IsButtonPressed()
 {
-
+	GPIO_PinState encButtonState;
+	encButtonState = HAL_GPIO_ReadPin(ENC_SW_GPIO_Port, ENC_SW_Pin);
+	if(encButtonState != GPIO_PIN_RESET)
+		return false;
+	return true;
 }
 
 
 static void EncoderTask()
 {
+	encoderMovement.dir = ENCODER_DIR_STOPPED;
+	encoderMovement.speed = ENCODER_SPEED_STOPPED;
+	encoderMovement.exactStepsDelta = 0;
 
 	while(true)
 	{
-
+		UpdateEncoderState();
+		LogEncoderParams();
+		vTaskDelay(TASK_DELAY_TIME);
 	}
+}
+
+static void UpdateEncoderState()
+{
+	static uint16_t oldCnt;
+	uint16_t steps = 0, newCnt = ENCODER_TIMER.Instance->CNT;
+	Encoder_Movement currentMovement;
+
+	if(newCnt > oldCnt)
+	{
+		steps = newCnt - oldCnt;
+		if(steps < ENCODER_TIMER_HALF_RANGE)	//no overflow
+		{
+			currentMovement.dir = ENCODER_DIR_RIGHT;
+		}
+		else	//overflow (difference too big for normal operation)
+		{
+			currentMovement.dir = ENCODER_DIR_LEFT;
+			steps = oldCnt - newCnt;
+		}
+	}
+	else if(newCnt < oldCnt)
+	{
+		steps = oldCnt - newCnt;
+		if(steps < ENCODER_TIMER_HALF_RANGE)	//no overflow
+		{
+			currentMovement.dir = ENCODER_DIR_LEFT;
+		}
+		else	//overflow
+		{
+			currentMovement.dir = ENCODER_DIR_RIGHT;
+			steps = newCnt - oldCnt;
+		}
+	}
+
+	steps /= 4;	//convert from ticks to steps
+	if(steps == 0)
+	{
+		currentMovement.dir = ENCODER_DIR_STOPPED;
+		currentMovement.speed = ENCODER_SPEED_STOPPED;
+	}
+	else
+	{
+		oldCnt = newCnt;	//update only if encoder is at stable position
+	}
+
+	currentMovement.exactStepsDelta = steps;
+
+	currentMovement.speed = ENCODER_SPEED_SLOW;	//TODO implement speed
+
+	taskENTER_CRITICAL();
+	encoderMovement = currentMovement;
+	taskEXIT_CRITICAL();
+}
+
+static void LogEncoderParams()
+{
+	if(encoderMovement.dir == ENCODER_DIR_STOPPED)
+		return;
+
+	const char * dirMsg;
+	if(encoderMovement.dir == ENCODER_DIR_LEFT)
+		dirMsg = "moving left";
+	else if(encoderMovement.dir == ENCODER_DIR_RIGHT)
+		dirMsg = "moving right";
+
+	Logging_Info("Encoder movement: ");
+	Logging_Log(dirMsg);
+	Logging_LogWithNum(", steps: ", encoderMovement.exactStepsDelta);
 }
