43638262a67c417fb2692dcf19d689f6fae2cf5e
Mateusz Chwast <chwastmat@gmail.com>
2023-01-22 16:58:33 +0100

    setup encoder gpio

--- Inc/main.h
+++ Inc/main.h
@@ -65,12 +65,18 @@ void Error_Handler(void);
 #define USART2_RX_GPIO_Port GPIOA
 #define NUCLEO_LED_Pin GPIO_PIN_5
 #define NUCLEO_LED_GPIO_Port GPIOA
+#define ENC_A_Pin GPIO_PIN_6
+#define ENC_A_GPIO_Port GPIOA
+#define ENC_B_Pin GPIO_PIN_7
+#define ENC_B_GPIO_Port GPIOA
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
 #define TCK_GPIO_Port GPIOA
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
+#define ENC_SW_Pin GPIO_PIN_6
+#define ENC_SW_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- Src/main.c
+++ Src/main.c
@@ -91,6 +91,7 @@ int main(void)
   MX_GPIO_Init();
   MX_USART2_UART_Init();
   MX_TIM6_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
   HAL_TIM_Base_Start_IT(&htim6);
 
